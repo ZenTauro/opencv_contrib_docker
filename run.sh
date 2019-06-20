e386c8db9223837c45de029f0eef9cd3512672c5
zentauro <pedrogomezmartin1999@gmail.com>
2019-06-20 02:55:57 +0200

    execute bash bugfix

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker run -it --rm \
     -e DISPLAY="${DISPLAY}" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /home/user/develop:"$(pwd)" \
-    opencv_contrib:1.0.0
+    opencv_contrib:1.0.0 \
     bash
